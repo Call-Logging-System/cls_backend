65486f7718b418ae3633df920897315da43bdc35
Rutvik Raut <rutvikr@cdac.in>
2026-02-20 14:42:39 +0530

    project init

---
+++ src/main/java/com/cdac/cls_services/ClsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.cdac.cls_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClsServicesApplication.class, args);
+	}
+
+}
