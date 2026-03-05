89a5f8839e6522d98483e2957f5ee3c5d4d2857f
Rutvik Raut <rutvikr@cdac.in>
2026-03-05 10:05:14 +0530

    enabled cors temporary

--- src/main/java/com/cdac/cls_services/call_logs/controller/CallLogsController.java
+++ src/main/java/com/cdac/cls_services/call_logs/controller/CallLogsController.java
@@ -4,12 +4,14 @@ import com.cdac.cls_services.call_logs.dto.CallLogsListDto;
 import com.cdac.cls_services.call_logs.models.CallLogModel;
 import com.cdac.cls_services.call_logs.service.CallLogsService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/call_logs")
 @AllArgsConstructor
